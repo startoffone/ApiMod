b690ec26f4d8cb192d3fe5774da0dc847394d881
startoffone <843233061@qq.com>
2022-07-09 06:25:06 +0800

    debug

--- src/main/java/ApiMod/helpers/ModHelper.java
+++ src/main/java/ApiMod/helpers/ModHelper.java
@@ -1,9 +1,13 @@
 package ApiMod.helpers;
 
+import ApiMod.power.Weapon;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public interface ModHelper {
+    Logger logger = LogManager.getLogger(ModHelper.class.getTypeName());
     //填写地址
     static String makePath(String path) {
         return "ApiMod/" + path;
@@ -11,6 +15,19 @@ public interface ModHelper {
     //填写id
     static String makeId(String id) {
         return "ApiMod:" + id;
+    }
+    static int checkWeapon(String name) {
+        switch (name) {
+            case "石镐":
+                return 1;
+            case "铁镐":
+                return 2;
+            case "钻石镐":
+                return 3;
+            default:
+                logger.info("[ERROR]WeaponType: " + name + " not found");
+                return 0;
+        }
     }
     //填写匿名action
     static void addToBottom(Lambda func) {
--- src/main/java/ApiMod/power/Weapon.java
+++ src/main/java/ApiMod/power/Weapon.java
@@ -7,31 +7,24 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Weapon extends Power {
     private static final String ID = ModHelper.makeId(Weapon.class.getSimpleName());
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(ID);
     private static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    private static final Logger logger = LogManager.getLogger(Weapon.class.getName());
-
     public Weapon(AbstractCreature Owner, String NAME) {
         super(ID, NAME, PowerType.BUFF, Owner);
-
     }
 
     @Override
     public void updateDescription() {
         //将武器名字拼接进描述
         this.description = String.format(DESCRIPTIONS[0], this.name);
-        int weaponType = checkWeapon();
+        int weaponType = ModHelper.checkWeapon(this.name);
         if (weaponType != 0) {
             this.description = this.description + String.format(DESCRIPTIONS[weaponType], weaponType);
         }
-
-
     }
 
     //增加能力时触发
@@ -39,31 +32,12 @@ public class Weapon extends Power {
     public void onApplyPower(AbstractPower power, AbstractCreature target, AbstractCreature source) {
         //武器牌唯一，判断是否使用武器
         if (power.ID.equals(ID)) {
-            String name = power.name;
+            int weaponType = ModHelper.checkWeapon(this.name);
             //取消之前的能力
-            if (checkWeapon()<4) {
-                pickaxeReduce(name, checkWeapon());
+            if (weaponType<4) {
+                addToBot(new ReducePowerAction(AbstractDungeon.player, AbstractDungeon.player, "ApiMod:Dig", weaponType));
+                this.name = power.name;//更新能力名称，为描述的更新定位
             }
         }
     }
-
-    protected void pickaxeReduce(String name, int amount) {
-        addToBot(new ReducePowerAction(AbstractDungeon.player, AbstractDungeon.player, "ApiMod:Dig", amount));
-        this.name = name;//更新能力名称，为描述的更新定位
-    }
-
-    private int checkWeapon() {
-        switch (this.name) {
-            case "石镐":
-                return 1;
-            case "铁镐":
-                return 2;
-            case "钻石镐":
-                return 3;
-            default:
-                logger.info("[ERROR]WeaponType: " + this.name + " not found");
-                return 0;
-        }
-    }
-
 }
